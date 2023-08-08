0624d7a7e1b037a0028ee55e9b09d882fed83e02
Dmitry <monk-time@users.noreply.github.com>
2023-08-08 19:40:55 +0300

    Add model Follow

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Follow, User
 
 
 @admin.register(User)
@@ -8,3 +8,8 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'username', 'first_name', 'last_name')
     list_display_links = ('email', 'username')
     list_filter = ('email', 'username')
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('follower', 'following')
--- backend/users/models.py
+++ backend/users/models.py
@@ -38,3 +38,38 @@ class User(AbstractUser):
         ordering = ('username',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Follow(models.Model):
+    """Модель подписок на пользователей."""
+
+    follower = models.ForeignKey(
+        User,
+        verbose_name='подписчик',
+        related_name='follower',
+        on_delete=models.CASCADE,
+    )
+    following = models.ForeignKey(
+        User,
+        verbose_name='подписан на',
+        related_name='following',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                name='unique_follow',
+                fields=['follower', 'following'],
+            ),
+            models.CheckConstraint(
+                name='cant_subscribe_to_self',
+                check=~models.Q(follower=models.F('following')),
+                violation_error_message='Нельзя подписаться на самого себя',
+            ),
+        ]
+
+    def __str__(self):
+        return f'{self.follower.username} -> {self.following.username}'
